9c5ee805345d9b7f80f0bd42baf92bbe8597ea65
T. William <ishimwewil005@gmail.com>
2019-10-14 11:54:55 +0200

    ch(heroku): enable process env port [Finishes 169118257]

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import http from 'http';
 import app from './app';
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port, () => console.log(`listening on port ${port}`));
