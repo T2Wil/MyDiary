057d76f68ab94698a1d74a34072e5b0a9c089d77
T. William <ishimwewil005@gmail.com>
2019-10-16 13:15:03 +0200

    feature(api-tests): add endpoints tests createEntry_tests.js deleteEntry_tests.js modifyEntry_tests.js signin_tests.js signup_tests.js viewEntries_tests.js viewSpecific_tests.js [Finishes 169177721]

---
+++ server/tests/createEntry_tests.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import chaiThings from 'chai-things';
+import fakeEntries from '../mock/fakeEntries';
+import app from '../app';
+
+process.env.NODE_ENV = 'test';
+
+
+chai.use(chaiHttp);
+chai.use(chaiThings);
+const { expect } = chai;
+
+describe('Test POST /api/v1/entries', () => {
+  let data = fakeEntries[0];
+  it('should return 200 HTTP status code if an entry successfully created', (done) => {
+    chai.request(app)
+      .post('/api/v1/entries/')
+      .send(data)
+      .end((err, res) => {
+        expect(res.body).to.have.property('status').equals(200).that.is.a('number');
+        expect(res.body).to.have.property('data').that.is.an('object');
+        expect(res.body).to.have.property('data').that.includes.property('id').that.is.a('number');
+        expect(res.body).to.have.property('data').that.includes.property('title').that.is.a('string');
+        expect(res.body).to.have.property('data').that.includes.property('description').that.is.a('string');
+        expect(res.body).to.have.property('data').that.includes.property('createdOn');
+        expect(res.body).to.have.property('data').that.includes.property('message').that.is.a('string');
+      });
+    done();
+  });
+  it('should return 400 HTTP status code if passing an empty entry', (done) => {
+    data = {};
+    chai.request(app)
+      .post('/api/v1/entries/')
+      .send(data)
+      .end((err, res) => {
+        expect(res.body).to.have.property('status').equals(400).that.is.a('number');
+        expect(res.body).to.have.property('error').equals('Bad request: Cant create an empty entry').that.is.a('string');
+      });
+    done();
+  });
+});
---
+++ server/tests/modifyEntry_tests.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import chaiThings from 'chai-things';
+import fakeEntries from '../mock/fakeEntries';
+import app from '../app';
+
+process.env.NODE_ENV = 'test';
+
+
+chai.use(chaiHttp);
+chai.use(chaiThings);
+const { expect } = chai;
+
+describe('Test PATCH /api/v1/entries/:entryId', () => {
+  const data = fakeEntries[0];
+  it('should return 200 HTTP status code if an entry successfully modified', (done) => {
+    chai.request(app)
+      .post('/api/v1/entries/')
+      .send(data)
+      .end((err, res) => {
+        const entryId = `${res.body.data.id}`;
+        const path = `/api/v1/entries/${entryId}`;
+        chai.request(app)
+          .patch(path)
+          .send(entryId)
+          .end((er, resp) => {
+            expect(resp.body).to.have.property('status').equals(200).that.is.a('number');
+            expect(resp.body).to.have.property('data').that.is.a('object');
+            expect(resp.body).to.have.property('data').that.includes.property('message').equals('entry successfully edited').that.is.a('string');
+            expect(resp.body).to.have.property('data').that.includes.property('id').that.is.a('number');
+            expect(resp.body).to.have.property('data').that.includes.property('title').that.is.a('string');
+            expect(resp.body).to.have.property('data').that.includes.property('description').that.is.a('string');
+          });
+        done();
+      });
+  });
+  it('should return 404 HTTP status code if entry doesn\'t exists', (done) => {
+    const path = '/api/v1/entries/10000';
+    chai.request(app)
+      .patch(path)
+      .send(data)
+      .end((err, res) => {
+        expect(res.body).to.have.property('status').equals(404).that.is.a('number');
+        expect(res.body).to.have.property('error').equals('Entry doesn\'t exists').that.is.a('string');
+      });
+    done();
+  });
+});
