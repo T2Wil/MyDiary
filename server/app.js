a0a510cfc6ea5542a3c2c413aca838953e06e033
T. William <ishimwewil005@gmail.com>
2019-10-23 12:33:48 +0200

    ft(API-documentation) Add documentation at API root /api/v1/ [ Finishes 169313627]

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/v1/', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/entries', entriesRoutes);
+app.use('/api/v1/', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 export default app;
