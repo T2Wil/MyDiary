9c2777bd9d88291ae6c5652b600bae746ec12770
T. William <ishimwewil005@gmail.com>
2019-10-25 13:45:31 +0200

    bg(UI-templates-login-page): Add spaces and visibility [Finishes 169089695]

---
+++ UI/js/confirmDelete.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-alert */
+const deleteIcon = document.querySelector('.delete-icon');
+
+deleteIcon.addEventListener('click', () => {
+  window.confirm('Are you sure you want to delete this permanently?');
+});
