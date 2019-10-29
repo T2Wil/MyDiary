041826c79202901f47bf3b017d295aa1752e7e07
T. William <ishimwewil005@gmail.com>
2019-10-29 14:26:29 +0200

    ft(UI-feedback) : Add view entry page add viewEntry.html add viewEntry.css Insert new line at the end of all files [ Finishes 169407688]

--- UI/js/confirmDelete.js
+++ UI/js/confirmDelete.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-alert */
 const deleteIcon = document.querySelector('.delete-icon');
 
 deleteIcon.addEventListener('click', () => {
-    window.confirm('Are you sure you want to delete this permanently?');
-}) ;
+  window.confirm('Are you sure you want to delete this permanently?');
+});
