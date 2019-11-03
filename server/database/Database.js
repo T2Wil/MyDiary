3d8f77661816835ddb2e590546057a22d0fba691
T. William <ishimwewil005@gmail.com>
2019-11-03 19:35:30 +0200

    ft(db-signup): add signup endpoint [Finishes 169520088]

---
+++ server/database/Database.js
@@ -0,0 +1,52 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+class Database {
+  constructor() {
+    this.createUsersTableReq = `CREATE TABLE IF NOT EXISTS 
+    users(
+      id UUID PRIMARY KEY,
+      firstName VARCHAR(128) NOT NULL,
+      lastName VARCHAR(128) NOT NULL,
+      email VARCHAR(128) UNIQUE NOT NULL,
+      password VARCHAR(128) UNIQUE NOT NULL
+      )`;
+    this.usersReq = 'SELECT * FROM users';
+    this.addUserReq = ' ';
+  }
+
+  async createUsersTable() {
+    const conn = await pool.connect();
+    const results = await conn.query(this.createUsersTableReq);
+    if (results) return true;
+    return false;
+  }
+
+  async addNewUser(user) {
+    this.addUserReq = `INSERT INTO 
+    users(id,firstname,lastname,email,password)
+    values('${user.id}','${user.fName}','${user.lName}','${user.email}','${user.pswd}'
+    )`;
+    const conn = await pool.connect();
+    const results = await conn.query(this.addUserReq);
+    if (results) return true;
+    return false;
+  }
+
+  async retrieveAllUsers() {
+    const conn = await pool.connect();
+    const results = await conn.query(this.usersReq);
+    return results.rows;
+  }
+}
+export default Database;
