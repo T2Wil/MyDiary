002604e4863e27cbf18286e2a15b29f6029500b8
T. William <ishimwewil005@gmail.com>
2019-11-04 14:27:32 +0200

    ch(db-endpoint): Modify db connection in controllers add signup controller [Finishes 169534480]

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,26 +10,10 @@ export const signup = async (req, res) => {
     const {
       firstName, lastName, email, password,
     } = req.body;
-    // const createUsersTableReq = `CREATE TABLE IF NOT EXISTS 
-    // users(
-    //   id UUID PRIMARY KEY,
-    //   firstName VARCHAR(128) NOT NULL,
-    //   lastName VARCHAR(128) NOT NULL,
-    //   email VARCHAR(128) UNIQUE NOT NULL,
-    //   password VARCHAR(128) NOT NULL
-    //   )`;
-    // const usersTableCreated = await pool.query(createUsersTableReq);
     const usersTableCreated = await database.createUsersTable();
-    // console.log(`usersTableCreated: ${JSON.stringify(usersTableCreated)}`);
     if (usersTableCreated) {
       user.setUser(firstName, lastName, password, email);
       const newUser = user.getUser();
-
-      // const addUserReq = `INSERT INTO 
-      // users(id,firstname,lastname,email,password)
-      // values('${newUser.id}','${newUser.fName}','${newUser.lName}','${newUser.email}','${newUser.pswd}'
-      // )`;
-      // const { rows } = await pool.query(addUserReq);
       await database.addUser(newUser);
       const { userId } = newUser;
       const newToken = generateToken({ userId });
