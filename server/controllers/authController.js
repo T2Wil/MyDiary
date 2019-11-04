ca7fba3a150e59007d6172db5c0e2c99eb6d679e
T. William <ishimwewil005@gmail.com>
2019-11-04 13:07:41 +0200

    ch(db-endpoint): Modify db connection in controllers add signup controller [Finishes 169534480]

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,93 +1,61 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 import User from '../models/User';
 import generateToken from '../helpers/generateToken';
-import Database from '../database/Database';
+
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
 
 const user = new User();
-const database = new Database();
 
 export const signup = async (req, res) => {
-  const {
-    firstName, lastName, email, password,
-  } = req.body;
-  const registeredUsers = await database.retrieveAllUsers();
-  if (registeredUsers) {
-    user.users = registeredUsers;
-    const userAlreadyExists = user.findUser(email);
-    if (userAlreadyExists) {
-      res.status(409).json({
-        status: res.statusCode,
-        error: 'Conflict: User Already exists',
-      });
-    } else {
+  try {
+    const {
+      firstName, lastName, email, password,
+    } = req.body;
+    const createUsersTableReq = `CREATE TABLE IF NOT EXISTS 
+    users(
+      id UUID PRIMARY KEY,
+      firstName VARCHAR(128) NOT NULL,
+      lastName VARCHAR(128) NOT NULL,
+      email VARCHAR(128) UNIQUE NOT NULL,
+      password VARCHAR(128) NOT NULL
+      )`;
+    const usersTableCreated = await pool.query(createUsersTableReq);
+    if (usersTableCreated) {
       user.setUser(firstName, lastName, password, email);
       const newUser = user.getUser();
-      const userAdded = await database.addNewUser(user);
-      if (userAdded) {
-        const { userId } = newUser;
-        const newToken = generateToken({ userId });
-        res.status(201).json({
-          status: res.statusCode,
-          message: 'User created successfully',
-          data: {
-            token: newToken,
-            userDetails: {
-              FirstName: user.fName,
-              LastName: user.lName,
-              Email: user.email,
-            },
-          },
-        });
-      }
-    }
-  } else if (!registeredUsers) {
-    user.setUser(firstName, lastName, password, email);
-    const newUser = user.getUser();
-    const tableCreated = await database.createUsersTable();
-    if (tableCreated) {
-      const userAdded = await database.addNewUser(user);
-      if (userAdded) {
-        const { userId } = newUser;
-        const newToken = generateToken({ userId });
-        res.status(201).json({
-          status: res.statusCode,
-          message: 'User created successfully',
-          data: {
-            token: newToken,
-            userDetails: {
-              FirstName: user.fName,
-              LastName: user.lName,
-              Email: user.email,
-            },
-          },
-        });
-      }
-    }
-  }
-};
 
-export const signin = (req, res) => {
-  const { email, password } = req.body;
-  const userInfo = user.findUser(email, password);
-  const userId = userInfo.id;
-  const newToken = generateToken({ userId });
-  if (userInfo) {
-    res.status(200).json({
-      status: res.statusCode,
-      message: 'User logged in successfully',
-      data: {
-        token: newToken,
-        userDetails: {
-          FirstName: user.fName,
-          LastName: user.lName,
-          Email: user.email,
+      const addUserReq = `INSERT INTO 
+      users(id,firstname,lastname,email,password)
+      values('${newUser.id}','${newUser.fName}','${newUser.lName}','${newUser.email}','${newUser.pswd}'
+      )`;
+      const { rows } = await pool.query(addUserReq);
+      const { userId } = newUser;
+      const newToken = generateToken({ userId });
+
+      res.status(201).json({
+        status: res.statusCode,
+        message: 'User created successfully',
+        data: {
+          token: newToken,
+          userDetails: {
+            FirstName: user.fName,
+            LastName: user.lName,
+            Email: user.email,
+          },
         },
-      },
-    });
-  } else {
-    res.status(401).json({
+      });
+    }
+  } catch(err) {
+    res.status(409).json({
       status: res.statusCode,
-      error: 'Invalid credentials',
+      error: 'Conflict: User Already exists',
     });
   }
 };
-export const { users } = user;
