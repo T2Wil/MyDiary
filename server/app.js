ffe86e6fc091f350a5c30f0207ccedfaade2a1bf
T. William <ishimwewil005@gmail.com>
2019-11-05 13:05:29 +0200

    (ft-signin): Add signin endpoint Add signin controller Modify User model Implement tests [Finishes 169520089]

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/entries', entriesRoutes);
-app.use('/api/v1/', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+app.use('/api/v2/auth', authRoutes);
+app.use('/api/v2/entries', entriesRoutes);
+app.use('/api/v2/', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 export default app;
