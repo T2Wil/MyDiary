8d03021af2aa616138a8e5c7918eff9fdcb91504
T. William <ishimwewil005@gmail.com>
2019-11-05 13:01:19 +0200

    (ft-signin): Add signin endpoint Add signin controller Modify User model Implement tests [Finishes 169520089]

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -70,18 +70,12 @@ export const signin = async (req, res) => {
             },
           },
         });
-      } else {
-        res.status(401).json({
-          status: res.statusCode,
-          error: 'Invalid credentials',
-        });
       }
-    } else {
-      res.status(401).json({
-        status: res.statusCode,
-        error: 'Invalid credentials',
-      });
     }
+    res.status(401).json({
+      status: res.statusCode,
+      error: 'Invalid credentials',
+    });
   } catch (err) {
     res.status(500).json({
       status: res.statusCode,
